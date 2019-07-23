01c261937fe48a73545a97119427eaf205acf2a5
Rohit Suthar <iamrohitsuthar@localhost.localdomain>
2019-07-23 17:32:53 +0530

    Add SignUp and SignIn Activity and change the MainSplash code and add the listener to buttons in main activity

--- src/com/chatroom/ui/MainSplash.java
+++ src/com/chatroom/ui/MainSplash.java
@@ -7,10 +7,11 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Image;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.border.LineBorder;
@@ -21,12 +22,13 @@ public class MainSplash {
 	private JFrame jFrame;
 	private JButton jBtnSignUp;
 	private JButton jBtnSignIn;
+	private Image iconLogo;
 
 	public MainSplash() throws IOException {
 		jFrame = new JFrame("CHATROOM");
 		
 		BufferedImage myImage1 = ImageIO.read(this.getClass().getResource("/logo.png"));
-		Image iconLogo = myImage1.getScaledInstance(150, 150, BufferedImage.SCALE_DEFAULT);
+		iconLogo = myImage1.getScaledInstance(150, 150, BufferedImage.SCALE_DEFAULT);
 		
 		jFrame.setContentPane(new JPanel() {
 			BufferedImage myImage = ImageIO.read(this.getClass().getResource("/background.jpg"));
@@ -37,12 +39,36 @@ public class MainSplash {
 		});
 		
 		jBtnSignUp = new JButton("Sign Up");
+		jBtnSignIn = new JButton("Sign In ");
+		
+		initializeAllWithProperties();
+
+	}
+	
+	private void ListeningEvents() {
+		jBtnSignUp.addActionListener(new ActionListener() {	
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				new SignUpActivity();
+				
+			}
+		});
+		
+		jBtnSignIn.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				new SignInActivity();
+			}
+		});
+	}
+	
+	private void initializeAllWithProperties() {
 		jBtnSignUp.setPreferredSize(new Dimension(150,35));
 		jBtnSignUp.setBackground(Color.WHITE);
 		jBtnSignUp.setBorder(new LineBorder(Color.blue, 3));
 		jBtnSignUp.setFocusPainted(false);
 		
-		jBtnSignIn = new JButton("Sign In ");
+		
 		jBtnSignIn.setPreferredSize(new Dimension(150,35));
 		jBtnSignIn.setBorder(new LineBorder(Color.blue, 3));
 		jBtnSignIn.setFocusPainted(false);
@@ -54,20 +80,26 @@ public class MainSplash {
 		c.anchor = GridBagConstraints.CENTER;
 		c.insets = logoInsets;
 		c.gridy = 1;
+		
 		jLabel = new JLabel(new ImageIcon(iconLogo));
 		jLabel.setBounds(0, 0, 100, 100);
 		jLabel.setPreferredSize(new Dimension(150,150));
 		jFrame.add(jLabel,c);
+		
 		c.gridx = 0;
 		c.gridy = 2;
 		c.insets = buttonInsets;
 		jFrame.add(jBtnSignUp,c);
+		
 		c.gridx = 0;
 		c.gridy = 3;
 		jFrame.add(jBtnSignIn,c);
+		
 		jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		jFrame.setSize(500,500);
 		jFrame.setVisible(true);
+		
+		ListeningEvents();
 	}
 	
 	public static void main(String args[]) throws IOException {
