13b93514c236272606a3913bd0a90e076ba7b2b4
Rohit Suthar <iamrohitsuthar@gmail.com>
2019-07-28 11:27:20 +0530

    Fix some warnings and typo

--- src/com/chatroom/ui/SignUpActivity.java
+++ src/com/chatroom/ui/SignUpActivity.java
@@ -33,6 +33,7 @@ public class SignUpActivity {
 	private CompoundBorder compoundBorderAfterClick;
 	private BufferedImage iconLogo;
 	
+	@SuppressWarnings("serial")
 	public SignUpActivity() throws IOException {
 		jFrame = new JFrame("CHATROOM Sign Up");
 		
@@ -48,13 +49,13 @@ public class SignUpActivity {
 			}
 		});
 		
-		//creating compound border for Text Field to specify left mergin to the text
+		//creating compound border for Text Field to specify left margin to the text
 		Border lineBorder = BorderFactory.createLineBorder(Color.blue, 1);
-		Border emptyBorder = new EmptyBorder(0,10,0,0); //left marign for text
+		Border emptyBorder = new EmptyBorder(0,10,0,0); //left margin for text
 		compoundBorder = new CompoundBorder(lineBorder,emptyBorder);
 		
-		Border lineBorder1 = BorderFactory.createLineBorder(Color.blue, 1);
-		Border emptyBorder1 = new EmptyBorder(0,10,0,0); //left marign for text
+		Border lineBorder1 = BorderFactory.createLineBorder(new Color(108, 99, 255), 3);
+		Border emptyBorder1 = new EmptyBorder(0,10,0,0); //left margin for text
 		compoundBorderAfterClick = new CompoundBorder(lineBorder1,emptyBorder1);
 		
 		jBtnSignUp = new JButton("Sign Up");
