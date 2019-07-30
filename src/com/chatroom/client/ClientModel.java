830e4c53c7ab67306466ba33790e557ed9a39a49
Rohit Suthar <iamrohitsuthar@gmail.com>
2019-07-30 19:24:18 +0530

    Implement connection with server

--- src/com/chatroom/client/ClientModel.java
+++ src/com/chatroom/client/ClientModel.java
@@ -1,22 +1,35 @@
 package com.chatroom.client;
 
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
 import java.net.Socket;
+
+import com.chatroom.others.LogFileWriter;
 
 public class ClientModel {
 	private int clientID=-1;
 	private int roomId = -1;
 	private String host = "";
 	private int port = -1;
-	private ObjectOutputStream objectOutputStream;
-	private ObjectInputStream objectInputStream;
+	public static ObjectOutputStream objectOutputStream;
+	public static ObjectInputStream objectInputStream;
 	private Socket socket;
 	
 	public ClientModel(String host, int port) {
 		super();
 		this.host = host;
 		this.port = port;
+		
+		// connecting
+		try {
+			socket = new Socket(host,port);
+			objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+			objectInputStream = new ObjectInputStream(socket.getInputStream());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public int getClientID() {
