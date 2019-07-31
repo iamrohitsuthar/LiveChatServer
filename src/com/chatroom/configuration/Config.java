c3beeb97d4c39b333c9fc060838b86faef8367ea
Rohit Suthar <iamrohitsuthar@gmail.com>
2019-07-31 17:33:09 +0530

    Now user have to pass four param to start the server - port no, database host, username and password

--- src/com/chatroom/configuration/Config.java
+++ src/com/chatroom/configuration/Config.java
@@ -2,14 +2,15 @@ package com.chatroom.configuration;
 
 import java.awt.Color;
 
-public interface Config {
-	public String USER_NAME  = "root";
-	public String USER_PWD = "WHOamiqwerty@123!";
-	public String DATABASE_URL = "jdbc:mysql://localhost:3306";
-	public String DATABASE_NAME = "chatroom";
-	public String TABLE_NAME = "users";
-	public String CLIENT_ID = "client_id";
-	public String CLIENT_NAME = "client_name";
-	public String CLIENT_PWD = "client_pwd";
-	public Color colorPrimary = new Color(108, 99, 255);
+public class Config {
+	public static String USER_NAME  = "";
+	public static String USER_PWD = "";
+	public static String DATABASE_HOST = "";
+	public static String DATABASE_URL = "jdbc:mysql://"+ DATABASE_HOST +":3306";
+	public static final String DATABASE_NAME = "chatroom";
+	public static final String TABLE_NAME = "users";
+	public static final String CLIENT_ID = "client_id";
+	public static final String CLIENT_NAME = "client_name";
+	public static final String CLIENT_PWD = "client_pwd";
+	public static final Color colorPrimary = new Color(108, 99, 255);
 }
--- src/com/chatroom/server/ServerExec.java
+++ src/com/chatroom/server/ServerExec.java
@@ -1,8 +1,14 @@
 package com.chatroom.server;
 
+import com.chatroom.configuration.Config;
+
 public class ServerExec {
 
 	public static void main(String[] args) {
+		//updating the configuration
+		Config.DATABASE_HOST = args[1];
+		Config.USER_NAME = args[2];
+		Config.USER_PWD = args[3];
 		Server server = new Server(Integer.parseInt(args[0]));
 		server.connect();
 	}
