1968b3f2ec4955d77e6fc7897aabfecda5f1526e
Rohit Suthar <iamrohitsuthar@gmail.com>
2019-08-02 17:14:16 +0530

    Add logs code in serverExec and ClientThread

--- src/com/chatroom/server/ServerExec.java
+++ src/com/chatroom/server/ServerExec.java
@@ -1,8 +1,30 @@
 package com.chatroom.server;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import com.chatroom.configuration.Config;
+import com.chatroom.others.LogFileWriter;
+
 public class ServerExec {
 
 	public static void main(String[] args) {
+		//create the log file if it is not present
+		String path = System.getProperty("user.home");
+		path += "/CHATROOM";
+		File file = new File(path);
+		if(!file.exists())
+			file.mkdir();
+				
+		File f = new File(System.getProperty("user.home")+"/CHATROOM/LOGS.txt");
+		if(!f.exists()) {
+			try {
+				f.createNewFile();
+			} catch (IOException e) {
+				e.printStackTrace(new PrintWriter(Config.errors));
+				LogFileWriter.Log(Config.errors.toString());
+			}
+		}
 		Server server = new Server(Integer.parseInt(args[0]));
 		server.connect();
 	}
