0ac728437022bbd08a63474ad590b449cc4002f5
Rohit Suthar <iamrohitsuthar@gmail.com>
2019-08-02 17:11:41 +0530

    add log code and add IF NOT EXISTS in table creation

--- src/com/chatroom/Database/createdb.java
+++ src/com/chatroom/Database/createdb.java
@@ -1,18 +1,14 @@
 package com.chatroom.Database;
 
 import java.io.PrintWriter;
-import java.io.StringWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
 import com.chatroom.configuration.Config;
 import com.chatroom.others.LogFileWriter;
-import com.chatroom.server.Server;
 
 
 public class createdb {
-	static StringWriter errors = new StringWriter();
 	public static void main(String[] args) {
 		Connection connection = null;
 		java.sql.Statement statement= null;
@@ -29,24 +25,24 @@ public class createdb {
 			
 			//for execute multiple queries separate queries by semicolon
 			connection = DriverManager.getConnection(Config.DATABASE_URL+'/'+Config.DATABASE_NAME+"?allowMultiQueries=true",Config.USER_NAME,Config.USER_PWD);
-			String Queries = "CREATE TABLE " + Config.TABLE_NAME + "(" + Config.CLIENT_ID + " int auto_increment," + Config.CLIENT_NAME + " VARCHAR(50) not null, "+ Config.CLIENT_PWD + " VARCHAR(150), " +"primary key(" +Config.CLIENT_ID+ "))";
+			String Queries = "CREATE TABLE IF NOT EXISTS " + Config.TABLE_NAME + "(" + Config.CLIENT_ID + " int auto_increment," + Config.CLIENT_NAME + " VARCHAR(50) not null, "+ Config.CLIENT_PWD + " VARCHAR(150), " +"primary key(" +Config.CLIENT_ID+ "))";
 			
 			statement = connection.createStatement();
 			statement.executeUpdate(Queries);
 			
 		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			LogFileWriter.Log(e.getMessage());
+			e.printStackTrace(new PrintWriter(Config.errors));
+			LogFileWriter.Log(Config.errors.toString());
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			LogFileWriter.Log(e.getMessage());
+			e.printStackTrace(new PrintWriter(Config.errors));
+			LogFileWriter.Log(Config.errors.toString());
 		}
 		finally {
 			try {
 				connection.close(); //close the database connection
 			} catch (SQLException e) {
-				e.printStackTrace(new PrintWriter(errors));
-				LogFileWriter.Log(errors.toString());
+				e.printStackTrace(new PrintWriter(Config.errors));
+				LogFileWriter.Log(Config.errors.toString());
 			}
 		}
 	}
